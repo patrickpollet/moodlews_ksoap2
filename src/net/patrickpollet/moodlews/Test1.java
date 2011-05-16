83d21466c29e299eda0ee8e7334ae3bdee1a977d
patrickpollet <pp@patrickpollet.net>
2011-05-16 11:14:16 +0200

    added the proper licensing text at the top of all classes : Apache2 for WSDL2java extension and genereal MIT type for others

--- src/net/patrickpollet/moodlews/Test1.java
+++ src/net/patrickpollet/moodlews/Test1.java
@@ -1,9 +1,31 @@
+/*
+ * Copyright (c) 2011 Patrick Pollet France
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the
+ * following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+ * LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO
+ * EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+ * USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * 
+ * Contributor(s): 
+ */
+
 package net.patrickpollet.moodlews;
 
 import java.util.Arrays;
 
 //use the new WSDL 
 import net.patrickpollet.moodlews.core.*;
+
 
 // definitions of Moodle server, login, password ...
 
@@ -128,6 +150,9 @@ public class Test1 {
 				ForumDiscussionRecord[] frd=moodle.get_forum_discussions(lr.getClient(), lr.getSessionkey(),93,5);
 				System.out.println (Arrays.toString(frd));
 				
+				AffectRecord ar=moodle.message_send(lr.getClient(), lr
+						.getSessionkey(),"ppollet","username","salut de moodlews_gson");
+				System.out.println ("resultat message"+ar);
 				
 				moodle.logout(lr.getClient(),lr.getSessionkey());
 				System.out.println ("bye");
--- src/org/ksoap2/wsdl/WSDL2ksoap.java
+++ src/org/ksoap2/wsdl/WSDL2ksoap.java
@@ -1,4 +1,5 @@
 /*
+ * Copyright 2011 Patrick Pollet 
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.ksoap2.wsdl;
 
 import org.apache.axis.wsdl.gen.Parser;
